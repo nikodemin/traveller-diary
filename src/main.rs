5473250b4dc378c7c25b4c40dab591cd7982c526
nikodemin <niko.demin@gmail.com>
2026-08-11 15:32:07 +0300

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use crate::dao::Dao;
 use rusqlite::Connection;
-use slint::{ModelRc, SharedString, VecModel, quit_event_loop, select_bundled_translation};
+use slint::{
+    CloseRequestResponse, ModelRc, SharedString, VecModel, quit_event_loop,
+    select_bundled_translation,
+};
 use std::error::Error;
 use std::rc::Rc;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 mod dao;
 mod model;
@@ -27,15 +30,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut conn = Connection::open("traveller_diary.db")?;
     let dao = Dao::new(&mut conn);
 
-    let exit = || {
+    let exit = Arc::new(Mutex::new(Some(|| {
+        println!("Exiting main window");
         conn.close().unwrap();
         quit_event_loop().unwrap()
-    };
+    })));
+    let exit2 = exit.clone();
 
     main_window.on_setLanguage(|lang| {
         select_bundled_translation(lang.as_str()).unwrap();
     });
-    //main_window.on_exit(|| exit());
+    main_window.on_exit(move || {
+        if let Ok(mut guard) = exit.lock() {
+            if let Some(f) = guard.take() {
+                f();
+            }
+        }
+    });
+    main_window.window().on_close_requested(move || {
+        if let Ok(mut guard) = exit2.lock() {
+            if let Some(f) = guard.take() {
+                f();
+            }
+        }
+        CloseRequestResponse::HideWindow
+    });
 
     main_window.run().map_err(Box::new)?;
     Ok(())
